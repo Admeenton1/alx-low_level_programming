925bced63f2ec73755092b4a6e59d630b8926c0f
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-10 09:38:05 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
  *main - Entry Point
  *Return: always 0 (success)
  */
-int main(voidi)
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)", sizeof(char));
 	printf("Size of an int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long char));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long char));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
