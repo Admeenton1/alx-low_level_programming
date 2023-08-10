d71f8f9081e7c4e8128745c8978e6e9832b44d71
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-10 09:07:00 -0700

    sizeof various

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *Return: always 0 (success)
+ */
+{
+	printf("size of a char: %d byte(s)", sizeof(char));
+	printf("size of an int: %d byte(s)", sizeof(int));
+	printf("size of a long int: %d byte(s)", sizeof(long char));
+	printf("size of a long long int: %d byte(s)", sizeof(long long char));
+	printf("size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
