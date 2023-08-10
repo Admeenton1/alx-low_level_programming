0de04684327ab96fc745961a0f59e1c530aa5017
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-10 09:12:07 -0700

    sizeof various

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  *main - Entry Point
  *Return: always 0 (success)
  */
+int main(void);
 {
 	printf("size of a char: %d byte(s)", sizeof(char));
 	printf("size of an int: %d byte(s)", sizeof(int));
