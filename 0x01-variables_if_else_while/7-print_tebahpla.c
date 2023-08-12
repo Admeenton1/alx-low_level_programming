92989c9a01cb7c360bf6acfabc6d49c942fe8de1
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-12 07:25:58 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int c;
+
+	for (c = 'z'; c <= 'a'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+
+}
