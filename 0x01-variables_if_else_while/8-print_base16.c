e8c753f9d60294128fc5f800d9401196493637b3
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-12 07:55:12 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int a;
+	char t;
+
+	for (a = '0'; a = '9'; a++)
+	putchar(a);
+
+	for (t = 'a'; t = 'f'; f++)
+	putchar(f);
+	putchar('\n');
+
+	return (0);
+
+}
