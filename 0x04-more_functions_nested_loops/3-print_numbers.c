b98b5a39d8701d67890525dcd831af1920a4373a
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-17 17:43:37 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers in a range
+ * Retrurn: The numbers sine 0 to 9
+ */
+{
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+	_putchar(k + '0');
+	}
+	_putchar('\n');
+}
