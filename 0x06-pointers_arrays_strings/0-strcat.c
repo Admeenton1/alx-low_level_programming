3a2c88acc148312306f221670502e56ea55d1c8f
Suleiman Adisa <mumeenadisa24@gmail.com>
2023-08-23 14:50:54 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenate the strings
+ * @dest: The destination
+ * @src: The source string
+ * Return: A pointet to the resulting string d
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int dlen = 0, 1;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
