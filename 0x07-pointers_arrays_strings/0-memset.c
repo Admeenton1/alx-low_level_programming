47a838ae5fed2f28dcdc070598f69da3e4231f75
Suleiman Adisa Mumeen <mumeenadisa24@gmail.com>
2023-08-29 00:32:05 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "mainh"
+
+/**
+ * _memse - Entry point
+ * @s: pointed destinaion
+ * @b: contant byte
+ * @n: bytes
+ * Reurn: Always 0 (success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
